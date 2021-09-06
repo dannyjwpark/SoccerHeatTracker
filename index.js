4f21bb6a85ca651e779c78eb600af8ceb8c1e334
Danny Park <danincali@gmail.com>
2021-09-06 10:11:18 -0700

    api fetching

--- index.js
+++ index.js
@@ -3,15 +3,37 @@ const dataURL = 'https://raw.githubusercontent.com/statsbomb/open-data/master/da
 // fetching JSON data from the source
 async function getSoccerData() {
     // const response = await fetch(dataURL);
-    // const data = await response.json();
-
+    // const data = await response.json();   
     fetch(dataURL)
-    .then((res) => res.json())
-    .then((data) => {
-        document.getElementById('output').innerHTML = data;
-    })
-    .catch((err) => console.log(err))
+        .then((res) => res.json())
+        .then((data) => console.log(data))
+        .catch((err) => console.log(err))
 };
 
 getSoccerData();
 
+// let dataButton = document.getElementById('showData');
+// if(dataButton){
+//     dataButton.addEventListener('click', getPassData);
+// }
+function getPassData(){
+    fetch(dataURL)
+        .then((res) => res.json())
+        .then((data) => {
+            let output = '<h2>Data</h2>';
+            data.forEach(function (datum) {
+                if(datum.type["name"] === "Pass"){
+                output += `
+                <ul>
+                    <li>ID: ${datum.id} </li>
+                    <li>time: ${datum.timestamp} </li>
+                    <li>team: ${datum.team["name"]} </li>
+                    <li>type: ${datum.type["name"]} </li>
+                </ul>
+                `;
+            }
+            });
+            document.getElementById('dataList').innerHTML = output;
+        })
+}
+getPassData()
