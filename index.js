4cc8680a75a63fb56274b4534fe147dd500b14c6
Danny Park <danincali@gmail.com>
2021-09-06 20:23:43 -0700

    package.json update

--- index.js
+++ index.js
@@ -27,12 +27,20 @@ document.getElementById('passData').addEventListener('click', dataTable);
 //         })
 // }
 
+function selectMode(){
+    const matchModes = document.getElementById('dropbtn');
+    let selectedMode = matchModes.options[matchModes.selectedIndex].value;
+    alert(selectedMode);
+    return(selectedMode);
+    
+}
+console.log(selectMode());
+
 function dataTable(){
     fetch(dataURL).then(
         res=> {
             res.json().then(
                 data=>{
-                    console.log(data);
                     // adjusting 2nd half minutes
                     data.forEach((el) => {
                         if (el.period === 2){
@@ -56,15 +64,18 @@ function dataTable(){
                                 temp += "<td>" + el.location + "</td>";
                                 temp += "<td>" + el.pass.end_location + "</td>";
                                 temp += "<td>" + el.pass.length + "</td>";
-                                temp += "<td>" + el.pass.length + "</td>";
                                 temp += "<td>" + el.pass.angle + "</td></tr>";
                             }
                         })
                         // --close for loop
 
-                        document.getElementById("data").innerHTML = temp;                    }
+                        let tablePos = document.getElementById("data");
+                        tablePos.insertAdjacentHTML('afterend',temp);                    
+                    }
                 }
-            )
+            ).catch(error => {
+                throw Error("ERROR: no data found");
+            })
         }
     )
 }
